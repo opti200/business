0c48e46a11a5e8cd28e40d4a8634a0628c5d6e61
kizito oure <optilanai25@gmail.com>
2026-02-21 15:14:37 +0300

    Add files via upload

---
+++ client.js
@@ -0,0 +1,8 @@
+// Smooth scroll for nav links
+document.querySelectorAll("a[href^='#']").forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
